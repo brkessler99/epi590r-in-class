31c8780c08e80a0c5bff04a881100287729fbae3
brk1112 <benjamin.kessler@emory.edu>
2023-08-14 11:56:07 -0400

    added some notes

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -2,8 +2,11 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "id", "nsibs", "samp", "race_eth", "sex", "region",
 							 "income", "res_1980", "res_2002", "age_bir")
 
+install.packages(rio)
+
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-in-class/data/raw/")
+#this doesn't work becasue we don't all have the same directory
+setwd("/Users/benjaminkessler/Desktop/epi590r/epi590r-in-class/data/raw")
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
@@ -18,5 +21,7 @@ nlsy <- nlsy |>
 
 nlsy <- na.omit(nlsy)
 
-setwd("../clean/")
+setwd("/Users/benjaminkessler/Desktop/epi590r/epi590r-in-class/data/clean")
 write_rds(nlsy, "nlsy-complete-cases.rds")
+
+#here::here only creates a file path
